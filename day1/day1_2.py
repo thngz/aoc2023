691c691c8369fda4facb7b7335607738802c101a
gkiviv <georgkivivali7@gmail.com>
2023-12-02 10:55:27 +0200

    improvements

--- day1/day1_2.py
+++ day1/day1_2.py
@@ -20,10 +20,14 @@ word_to_digit = {
     "eight": "8",
     "nine": "9",
 }
-
-for line in lines:
-    digits = re.findall(r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))", line)
-    result += int(word_to_digit.get(digits[0], digits[0]) + word_to_digit.get(digits[-1], digits[-1]))
-
-print(result)
-
+print(sum([int(word_to_digit.get(re.findall(r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))", line)[0],
+                                 re.findall(r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))", line)[0]) +
+                                 word_to_digit.get(re.findall(r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))", line)[-1],
+                                 re.findall(r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))", line)[-1])) for line in lines]))
+# print(sum([int((wtd := {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}).get(res := (re.findall(r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))", line))[0], res[0]) + wtd.get(res[-1], res[-1])) for line in lines]))
+#
+# print(sum([int(word_to_digit.get(res := (re.findall(r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))", line))[0], res[0]) + word_to_digit.get(res[-1], res[-1])) for line in lines]))
+# for line in lines:
+#     digits = re.findall(r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))", line)
+#     result += int(word_to_digit.get(digits[0], digits[0]) + word_to_digit.get(digits[-1], digits[-1]))
+#
--- day2/day2_1.py
+++ day2/day2_1.py
@@ -13,14 +13,6 @@ with open("input.txt", "r") as f:
 #     "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
 # ]
 
-
-class Game:
-    def __init__(self, id, bc, rc, gc):
-        self.id = id
-        self.bc = bc  # blue count
-        self.rc = rc  # red count
-        self.gc = gc  # green count
-
 games = []
 
 for game_line in input:
@@ -28,36 +20,25 @@ for game_line in input:
     game_name = int(game_name.split(" ")[1])
     color_matches = re.finditer(r"(\d+ blue)|(\d+ red)|(\d+ green)", game_results)
 
-    blue_count = 0
-    red_count = 0
-    green_count = 0
+    invalid_game = False
 
     for match in color_matches:
         blue_match = match.group(1)
         red_match = match.group(2)
         green_match = match.group(3)
 
-        if blue_match:
-            blues = int(blue_match.split(" ")[0])
-            if (blues > 14):
-                blue_count = 0
-                break
+        if not invalid_game:
+            if blue_match:
+                blues = int(blue_match.split(" ")[0])
+                invalid_game = blues > 14
+            if red_match:
+                reds = int(red_match.split(" ")[0])
+                invalid_game = reds > 12
+            if green_match:
+                greens = int(green_match.split(" ")[0])
+                invalid_game = greens > 13
 
-            blue_count += blues
-        if red_match:
-            reds = int(red_match.split(" ")[0])
-            if (reds > 12):
-                red_count = 0
-                break
-            red_count += reds
-        if green_match:
-            greens = int(green_match.split(" ")[0])
-            if (greens > 13):
-                green_count = 0
-                break
-            green_count += greens
-    if blue_count and red_count and green_count:
-        game = Game(game_name, blue_count, red_count, green_count)
-        games.append(game)
+    if not invalid_game:
+        games.append(game_name)
 
-print(sum([game.id for game in games]))
+print(sum([game_id for game_id in games]))
